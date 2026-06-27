b08f8d845df2121aebdb369860258a21792ed57f
simonhtjl <simonhtjl@gmail.com>
2026-06-27 18:56:19 +0700

    fixing config db

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 BASE_URL = "https://api.bmkg.go.id/publik/prakiraan-cuaca"
 ADM4 = "12.17.08.2021"
+
 DB_CONFIG = {
     "host": "localhost",
     "database": "bmkg_db",
-    "user": "postgres",
+    "user": "bmkg_user",
     "password": "postgres"
 }
